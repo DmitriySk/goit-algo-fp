6e8a3cede049e13e150d167ee44f1ae30bb8cf5b
Dmytro Sk. <dima.skalskij@gmail.com>
2026-05-24 20:45:11 +0300

    task 4

---
+++ task4.py
@@ -0,0 +1,85 @@
+import uuid
+import networkx as nx
+import matplotlib.pyplot as plt
+import heapq
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4()) # Унікальний ідентифікатор для кожного вузла
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val) # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)} # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+heapq.heapify(nums)
+colors = [
+    "#1874CD",
+    "#2E86C1",
+    "#3498DB",
+    "#5DADE2",
+    "#85C1E9",
+    "#A9CCE3",
+    "#2ECC71",
+    "#27AE60",
+    "#1E8449",
+    "#58D68D",
+    "#82E0AA",
+    "#A9DFBF",
+    "#F7DC6F",
+    "#F0B27A",
+    "#EC7063",
+]
+
+def build_heap(values):
+    if not values:
+        return None
+
+    heap = values[:]
+    heapq.heapify(heap)
+
+    nodes = [Node(val, color=colors[i % len(colors)]) for i, val in enumerate(heap)]
+
+    for i in range(len(nodes)):
+        left_idx  = 2 * i + 1
+        right_idx = 2 * i + 2
+
+        if left_idx < len(nodes):
+            nodes[i].left = nodes[left_idx]
+        if right_idx < len(nodes):
+            nodes[i].right = nodes[right_idx]
+
+    return nodes[0]
+
+root = build_heap(nums)
+
+draw_tree(root)
